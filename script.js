3000ec3581b3d68fcbe0265cf95a7c6a68de3e74
mokadde <mokaddes.ru2000@gmail.com>
2024-09-10 21:31:42 +0600

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle button functionality for mobile view
+document.getElementById('toggle-button').addEventListener('click', function () {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active');
+});
